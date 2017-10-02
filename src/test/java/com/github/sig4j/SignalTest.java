ad59273a9214c0b80bc0e0a9d6c3ff5c80bc0db0
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2017-10-02 16:51:19 +0200

    Enhanced tests.

--- src/test/java/com/github/sig4j/SignalTest.java
+++ src/test/java/com/github/sig4j/SignalTest.java
@@ -8,167 +8,168 @@ import static org.junit.Assert.*;
 
 public class SignalTest {
 
-    private static class SignalProvider {
-        Signal1<String> stringSignal1 = new Signal1<>();
-        Signal4<Integer, Integer, Integer, Integer> intSignal4 = new Signal4<>();
-    }
+	private static class SignalProvider {
+		Signal1<String> stringSignal1 = new Signal1<>();
+		Signal4<Integer, Integer, Integer, Integer> intSignal4 = new Signal4<>();
+	}
 
-    private static class SlotProvider {
+	private static class SlotProvider {
+		@SuppressWarnings("unused")
         void stringSlot1(final String s) {}
-    }
+	}
 
 
-    @Test
-    public void implicitDirect() {
-        SignalProvider signalProvider = new SignalProvider();
-        SlotProvider slotProvider = new SlotProvider();
-        signalProvider.stringSignal1.connect(slotProvider::stringSlot1);
-    }
+	@Test
+	public void implicitDirect() {
+		SignalProvider signalProvider = new SignalProvider();
+		SlotProvider slotProvider = new SlotProvider();
+		signalProvider.stringSignal1.connect(slotProvider::stringSlot1);
+	}
 
-    @Test
-    public void implicitDirectLambda() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(s -> {});
-    }
+	@Test
+	public void implicitDirectLambda() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(s -> {});
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void implicitDirectNull() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(null);
-    }
-
-
-
-    @Test
-    public void explicitDirect() {
-        SignalProvider signalProvider = new SignalProvider();
-        SlotProvider slotProvider = new SlotProvider();
-        signalProvider.stringSignal1.connect(
-                slotProvider::stringSlot1, Type.DIRECT);
-    }
-
-    @Test
-    public void explicitDirectLambda() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(s -> {}, Type.DIRECT);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void explicitDirectNull() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(null, Type.DIRECT);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void implicitDirectNull() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(null);
+	}
 
 
 
-    @Test
-    public void queued() {
-        SignalProvider signalProvider = new SignalProvider();
-        SlotProvider slotProvider = new SlotProvider();
-        signalProvider.stringSignal1.connect(
-                slotProvider::stringSlot1, Type.QUEUED);
-    }
+	@Test
+	public void explicitDirect() {
+		SignalProvider signalProvider = new SignalProvider();
+		SlotProvider slotProvider = new SlotProvider();
+		signalProvider.stringSignal1.connect(
+				slotProvider::stringSlot1, Type.DIRECT);
+	}
 
-    @Test
-    public void queuedLambda() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(s -> {}, Type.QUEUED);
-    }
+	@Test
+	public void explicitDirectLambda() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(s -> {}, Type.DIRECT);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void queuedNull() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(null, Type.QUEUED);
-    }
-
-
-
-    @Test
-    public void emitString1() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(s ->
-                assertEquals("test", s));
-        signalProvider.stringSignal1.emit("test");
-    }
-
-    @Test
-    public void emitInteger4() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.intSignal4.connect(
-                (i, ii, iii, iiii) -> {
-                    assertEquals(new Integer(0), i);
-                    assertEquals(new Integer(1), ii);
-                    assertEquals(new Integer(2), iii);
-                    assertEquals(new Integer(-3), iiii);
-                });
-        signalProvider.intSignal4.emit(0, 1, 2, -3);
-    }
-
-    @Test
-    public void emitNull4() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.intSignal4.connect(
-                (i, ii, iii, iiii) -> {
-                    assertNull(i);
-                    assertNull(ii);
-                    assertNull(iii);
-                    assertNull(iiii);
-                });
-        signalProvider.intSignal4.emit(null, null, null, null);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void explicitDirectNull() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(null, Type.DIRECT);
+	}
 
 
 
-    @Test(expected = RuntimeException.class)
-    public void enabledDefault() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.emit("enabled");
-    }
+	@Test
+	public void queued() {
+		SignalProvider signalProvider = new SignalProvider();
+		SlotProvider slotProvider = new SlotProvider();
+		signalProvider.stringSignal1.connect(
+				slotProvider::stringSlot1, Type.QUEUED);
+	}
 
-    @Test(expected = RuntimeException.class)
-    public void enabled() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.disable();
-        signalProvider.stringSignal1.enable();
-        signalProvider.stringSignal1.emit("enabled");
-    }
+	@Test
+	public void queuedLambda() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(s -> {}, Type.QUEUED);
+	}
 
-    @Test
-    public void disabled() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.disable();
-        signalProvider.stringSignal1.emit("enabled");
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void queuedNull() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(null, Type.QUEUED);
+	}
 
 
 
-    @Test
-    public void clear() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.clear();
-        signalProvider.stringSignal1.emit("enabled");
-    }
+	@Test
+	public void emitString1() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(s ->
+				assertEquals("test", s));
+		signalProvider.stringSignal1.emit("test");
+	}
 
-    @Test
-    public void clearMulti() {
-        SignalProvider signalProvider = new SignalProvider();
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.connect(
-                s -> {throw new RuntimeException();});
-        signalProvider.stringSignal1.clear();
-        signalProvider.stringSignal1.emit("enabled");
-    }
+	@Test
+	public void emitInteger4() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.intSignal4.connect(
+				(i, ii, iii, iiii) -> {
+					assertEquals(new Integer(0), i);
+					assertEquals(new Integer(1), ii);
+					assertEquals(new Integer(2), iii);
+					assertEquals(new Integer(-3), iiii);
+				});
+		signalProvider.intSignal4.emit(0, 1, 2, -3);
+	}
+
+	@Test
+	public void emitNull4() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.intSignal4.connect(
+				(i, ii, iii, iiii) -> {
+					assertNull(i);
+					assertNull(ii);
+					assertNull(iii);
+					assertNull(iiii);
+				});
+		signalProvider.intSignal4.emit(null, null, null, null);
+	}
+
+
+
+	@Test(expected = RuntimeException.class)
+	public void enabledDefault() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.emit("enabled");
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void enabled() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.disable();
+		signalProvider.stringSignal1.enable();
+		signalProvider.stringSignal1.emit("enabled");
+	}
+
+	@Test
+	public void disabled() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.disable();
+		signalProvider.stringSignal1.emit("enabled");
+	}
+
+
+
+	@Test
+	public void clear() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.clear();
+		signalProvider.stringSignal1.emit("enabled");
+	}
+
+	@Test
+	public void clearMulti() {
+		SignalProvider signalProvider = new SignalProvider();
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.connect(
+				s -> {throw new RuntimeException();});
+		signalProvider.stringSignal1.clear();
+		signalProvider.stringSignal1.emit("enabled");
+	}
 }
