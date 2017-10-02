95547bbdc6875967a41fdb85e8f9afde32c3c7b9
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2017-10-02 16:50:39 +0200

    Enhanced examples.

--- src/main/java/com/github/sig4j/example/ConsumerProducer.java
+++ src/main/java/com/github/sig4j/example/ConsumerProducer.java
@@ -6,35 +6,35 @@ import com.github.sig4j.signal.Signal1;
 import java.util.Scanner;
 
 public class ConsumerProducer {
-    public static void main(final String[] args) {
-        Producer p = new Producer();
-        Consumer c = new Consumer();
-        p.signal.connect(c::print, c);
-        p.signal.connect(c::printReverse, c);
+	public static void main(final String[] args) {
+		Producer p = new Producer();
+		Consumer c = new Consumer();
+		p.signal.connect(c::print, c);
+		p.signal.connect(c::printReverse, c);
 
-        new Thread(p::start).start();
-        new Thread(c::run).start();
-    }
+		new Thread(p::start).start();
+		new Thread(c::run).start();
+	}
 
-    private static final class Producer {
-        public final Signal1<String> signal = new Signal1<>();
-        private final Scanner scanner = new Scanner(System.in);
+	private static final class Producer {
+		public final Signal1<String> signal = new Signal1<>();
+		private final Scanner scanner = new Scanner(System.in);
 
-        void start() {
-            System.out.println("Write something:");
-            while (true) {
-                signal.emit(scanner.nextLine());
-            }
-        }
-    }
+		void start() {
+			System.out.println("Write something:");
+			while (true) {
+				signal.emit(scanner.nextLine());
+			}
+		}
+	}
 
-    private static final class Consumer extends Dispatcher {
-        void print(final String input) {
-            System.out.println(input);
-        }
+	private static final class Consumer extends Dispatcher {
+		void print(final String input) {
+			System.out.println(input);
+		}
 
-        void printReverse(final String input) {
-            System.out.println(new StringBuilder(input).reverse().toString());
-        }
-    }
+		void printReverse(final String input) {
+			System.out.println(new StringBuilder(input).reverse().toString());
+		}
+	}
 }
--- src/main/java/com/github/sig4j/example/QueuedExample.java
+++ src/main/java/com/github/sig4j/example/QueuedExample.java
@@ -6,22 +6,22 @@ import com.github.sig4j.signal.Signal0;
 
 public class QueuedExample {
 
-    public static void main(final String[] args) throws InterruptedException {
-        A a = new A();
-        a.signal.connect(() -> {
-            try {
-                Thread.sleep(1000);
-                System.out.println("this should be the last line");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, Type.QUEUED);
-        a.signal.emit();
-        System.out.println("this should be the first line");
-        Thread.sleep(2000);
-    }
+	public static void main(final String[] args) throws InterruptedException {
+		A a = new A();
+		a.signal.connect(() -> {
+			try {
+				Thread.sleep(1000);
+				System.out.println("this should be the last line");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}, Type.QUEUED);
+		a.signal.emit();
+		System.out.println("this should be the first line");
+		Thread.sleep(2000);
+	}
 
-    private static class A {
-        public final Signal0 signal = new Signal0();
-    }
+	private static class A {
+		public final Signal0 signal = new Signal0();
+	}
 }
